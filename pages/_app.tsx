386297dabfb9a02ba3ad20e4675e04f31db9e536
Sinan Karakaya <sinan.karakaya@epitech.eu>
2023-04-05 21:40:05 +0200

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { NextUIProvider, createTheme } from '@nextui-org/react'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const darkMode = createTheme({
+    type: 'dark'
+  })
+
+  return (
+    <NextUIProvider theme={darkMode}>
+     <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
